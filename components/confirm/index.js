711e13fbd9b5115c81b9c51073d7386d9af393db
heqiong <376497829@qq.com>
2018-05-18 18:17:01 +0800

    成功后的提示层组件

---
+++ components/confirm/index.js
@@ -0,0 +1,50 @@
+// components/ confirm/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    showConfirm: {
+      type: Boolean,
+      value: false
+    },
+    showIcon: {
+      type: Boolean,
+      value: false
+    },
+    title: {
+      type: String,
+      value: ''
+    },
+    message: {
+      type: String,
+      value: ''
+    },
+    showCancel: {
+      type: Boolean,
+      value: true
+    },
+    cancelText: {
+      type: String,
+      value: '取消'
+    },
+    okText: {
+      type: String,
+      value: '确定'
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
