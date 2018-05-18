e06472af8e50507321969c2fc595a3b464da09b3
heqiong <376497829@qq.com>
2018-05-18 18:17:41 +0800

    多选组件

---
+++ components/check-box/index.js
@@ -0,0 +1,28 @@
+// components/check-box/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    label: String,
+    color: String,
+    checked: {
+      type: Boolean,
+      value: false
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
