59af153af745b17bf61289acf962257a02c89a33
chenxiangjun <506402424@qq.com>
2018-05-22 17:20:52 +0800

    增加请求头

--- pages/runxin-manage/index.js
+++ pages/runxin-manage/index.js
@@ -15,23 +15,23 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
-  goto: function() {
+  goto: function () {
     wx.navigateTo({
       url: '/pages/runxin-distribute/index',
     });
   },
 
-  tabClick: function(e) {
+  tabClick: function (e) {
     let id = e.currentTarget.dataset.id;
     this.setData({
       activeCategoryId: id
@@ -39,16 +39,17 @@ Page({
 
     wx.request({
       url: config.prefix,
+      header: { 'Content-Type': 'application/json' },
       data: {
         serviceCode: 'BILL0001'
       },
-      success: function(res){
+      success: function (res) {
         console.log(res);
       }
     })
   },
 
-  tabTypeClick: function(e) {
+  tabTypeClick: function (e) {
     let id = e.currentTarget.dataset.id;
     this.setData({
       typeId: id
@@ -59,41 +60,41 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
