818f8567b6bfd070a39546f58bd3a6399ed3e1f6
chen <xiangjun@chens-iMac.lan>
2018-05-24 09:32:15 -0700

    润新签发数据联调

--- pages/runxin-distribute/index.js
+++ pages/runxin-distribute/index.js
@@ -1,65 +1,93 @@
+var config = require('../../utils/config.js');
+var util = require('../../utils/util.js');
+var app = getApp();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    receivingData: [],
+    receivingNameArry: [],
+    receivingCodeArry :  [],
+    receivingName : '请选择',
+    dateSelectV: '请选择',
+    receivingCode:  '',
+    accNo: '',
+    bankName: '',
+    branchName:'',
+    openDate: '',
+    expireDate:''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    let nowDate = new Date();
+    this.openDate = util.formatTime(nowDate);
+    this.requestData();
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  bindPickerChange: function (event) {
+    let index = event.detail.value;
+    let receivingName = this.data.receivingNameArry[index];
+    let receivingCode = this.data.receivingCodeArry[index];
+
+    let currentReceiving = this.data.receivingData.filter(function (v, n) {
+      return v.accNo === receivingCode
+    });
+
+    this.setData({
+      receivingName: receivingName,
+      receivingCode: receivingCode,
+      bankName: currentReceiving[0].bankName,
+      branchName: currentReceiving[0].branchName,
+      accNo: currentReceiving[0].accNo
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+  bindDateChange: function (event) {
+    let val = event.detail.value;
+    let openDateTime = +(new Date(this.data.openDate));
+    let expireDateTime = +(new Date(val));
+
+    debugger
+    this.setData({
+      expireDate: val
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
+  requestData() {
+    let that = this;
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
+    wx.request({
+      url: config.prefix,
+      method: 'POST',
+      data: {
+        serviceCode: 'BASE0008',
+        bizType: '1'
+      },
+      success: function(res) {
+        if (res.data.respCode === '0000') {
+          let datas = res.data.receEntList;
+          let receivingNameArry = [];
+          let receivingCodeArry = [];
+          
+          for (let i = 0, len = datas.length; i <len; i++) {
+            receivingNameArry.push(datas[i].entName);
+            receivingCodeArry.push(datas[i].accNo);
+          }
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+          that.setData({
+            receivingData: datas,
+            receivingNameArry: receivingNameArry,
+            receivingCodeArry: receivingCodeArry,
+            openDate: that.openDate
+          });
+        }
+      }
+    })
   }
 })
